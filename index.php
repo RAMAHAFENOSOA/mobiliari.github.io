f055d61d557e1515da4180e437909489c3f9cb22
Fiderana Ny Avo RAMAHAFENOSOA <you@example.com>
2024-08-10 19:15:02 +0300

    update php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Mobiliari</title>
+</head>
+
+<body>
+    <?php include "./includes/header.php" ?>
+    <?php include "./includes/about.php" ?>
+    <?php include "./includes/products.php" ?>
+    <?php include "./includes/sale.php" ?>
+    <?php include "./includes/contact.php" ?>
+    <script src="./script.js"></script>
+    <?php include "./includes/footer.php" ?>
+</body>
+
+</html>
